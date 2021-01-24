08814bf56d7d7a9983c4d756a435a3816d5a43d5
hesham <hesham@heshams-iMac.local>
2021-01-24 10:38:48 +0300

    whatever

---
+++ src/data.js
@@ -0,0 +1,20 @@
+const instrocturs ={
+    id: 1,
+    Emoji: "🌚",
+    Name: "Hamza",
+    GoToGithub: "Go to GitHub",
+  },
+  {
+    id: 2,
+    Emoji: "🐥",
+    Name: "Laila",
+    GoToGithub: "Go to GitHub",
+  },
+  {
+    id: 3,
+    Emoji: "🦍",
+    Name: "Hasan",
+    GoToGithub: "Go to GitHub",
+  },
+];
+export default instrocturs;
