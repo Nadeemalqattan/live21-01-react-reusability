ae90220bafd5fa8c34ee3cb8a61079c8198bc9d1
hesham <hesham@heshams-iMac.local>
2021-01-25 08:32:22 +0300

    got stuckkk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import "./App.css";
+import InstructorTag from "./components/InstructorTag.js";
+import { AppWrapper } from "./components/styles.js";
+
+const instrocturs = [
+  {
+    id: 1,
+    Emoji: "🌚",
+    Name: "Hamza",
+    github: "DarthHamza",
+  },
+  {
+    id: 2,
+    Emoji: "🐥",
+    Name: "Laila",
+    github: "Lailz",
+  },
+  {
+    id: 3,
+    Emoji: "🦍",
+    Name: "Hasan",
+    github: "thehasanas",
+  },
+];
 
 const App = () => {
+  const instroctors = data.map((Instructor) => (
+    <InstructorTag
+      key={Instructor.id}
+      name={Instructor.name}
+      github={Instructor.github}
+      emoji={Instructor.emoji}
+    />
+  ));
   return (
-    <div className="AppWrapper">
+    <AppWrapper>
       <h2>When in doubt, ask for help!</h2>
+      <div>{instroctors}</div>
+    </AppWrapper>
+  );
+};
 
-      <div
+export default instrocturs;
+
+{
+  /* <div
         className="TagWrapper"
         onClick={() => window.open(`https://github.com/DarthHamza`)}
       >
@@ -30,9 +67,5 @@ const App = () => {
         <span className="Emoji">🦍</span>
         <span className="Name">Hasan</span>
         <span className="GoToGithub">Go to GitHub</span>
-      </div>
-    </div>
-  );
-};
-
-export default App;
+      </div> */
+}
--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,14 @@
-const InstructorTag = () => {
-  // Create the InstructorTag component here
-
-  return <></>;
+import { TagWrapper, Emoji, Name, GoToGithub } from "../styles";
+const InstructorTag = (props) => {
+  return (
+    <TagWrapper
+      onClick={() => window.open(`https://github.com/${props.github}`)}
+    >
+      <Emoji>{instructor.emoji}</Emoji>
+      <Name>{instructor.name}</Name>
+      <GoToGithub>{instructor.github}</GoToGithub>
+    </TagWrapper>
+  );
 };
 
 export default InstructorTag;
